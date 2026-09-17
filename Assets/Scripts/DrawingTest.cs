62ac8cdac1f6d2cc77d1d21b6eea71e2c6147ca5
Tuulia Arvola <112947716+tuuliaarvola@users.noreply.github.com>
2026-09-17 19:51:24 +0300

    koodi drawingtest lisätty

--- Assets/Scripts/DrawingTest.cs
+++ Assets/Scripts/DrawingTest.cs
@@ -1,41 +1,81 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
 public class DrawingTest : MonoBehaviour
 {
+
     public GameObject VectorTo;
 
+    [Header("Screen Attributes")]
+    [Range(640, 1920)]
+    public int ScreenWidth = 1920;
+    [Range(480, 1080)]
+    public int ScreenHeight = 1080;
+
+    [Range(0, 1920)]
+    public int ScreenPositionX = 0;
+    [Range(0, 1080)]
+    public int ScreenPositionY = 0;
+
+    [Header("Popup Attributes")]
+    [Range(0, 100)]
+    public int PopupPercentageWidth = 75;
+    [Range(0, 100)]
+    public int PopupPercentageHeight = 75;
+
+    [Header("Healthbar Attributes")]
+    [Range(-50, 50)]
+    public int HealthBarOffsetX = 10;
+    [Range(-50, 50)]
+    public int HealthBarOffsetY = 4;
+
+    [Range(0, 50)]
+    public int HealthBarWidth = 12;
+    [Range(0, 50)]
+    public int HealthBarHeight = 2;
 
     private void OnDrawGizmos()
     {
         Vector3 v = VectorTo.transform.position;
-        Handles.color = Color.red;
-        // start from the origin, end at VectorTo.transform.position
-        Handles.DrawLine(Vector3.zero, v, 3f);
+        // Call the static method from Drawing-class
+        Drawing.DrawVector(v, Vector3.zero, Color.magenta, 3f);
+        // Call the static method for drawing the rectangle
+        Vector2 screenpos = new Vector2(ScreenPositionX, ScreenPositionY);
+        Vector2 screensize = new Vector2(ScreenWidth, ScreenHeight);
+        Drawing.DrawXYRectangle(screenpos,
+                                screensize,
+                                Color.white, 3f);
 
-        Handles.ConeHandleCap(0, v-v.normalized*0.35f, Quaternion.LookRotation(v), 0.5f, EventType.Repaint);
-        Handles.color = Color.white;
-        Handles.DrawLine(new Vector3(0, 0, 0), new Vector3(0, 1080, 0), 3f);
+        // Popup
+        Vector2 popupsize = new Vector2(ScreenWidth * PopupPercentageWidth / 100,
+                                        ScreenHeight * PopupPercentageHeight / 100);
 
-        Handles.DrawLine(new Vector3(0, 0, 0), new Vector3(1920, 0, 0), 3f);
+        Vector2 popupoffset = new Vector2(ScreenWidth * (100 - PopupPercentageWidth) / 200,
+                                          ScreenHeight * (100 - PopupPercentageHeight) / 200);
 
-        Handles.DrawLine(new Vector3(1920, 0, 0), new Vector3(1920, 1080, 0), 3f);
+        Drawing.DrawXYRectangle(screenpos + popupoffset, popupsize, Color.white, 3f);
 
-        Handles.DrawLine(new Vector3(1920, 1080, 0), new Vector3(0, 1080, 0), 3f);
-        
-        
+        // Healthbar
+        Vector2 hb_offset = new Vector2(ScreenWidth * HealthBarOffsetX / 100,
+                                        ScreenHeight * HealthBarOffsetY / 100);
+        Vector2 hb_size = new Vector2(ScreenWidth * HealthBarWidth / 100,
+                                        ScreenHeight * HealthBarHeight / 100);
+
+        Drawing.DrawXYRectangle(screenpos + screensize - hb_offset, hb_size, Color.darkGreen, 3f);
+
+
     }
-    // Start is called before the first frame update
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
+
